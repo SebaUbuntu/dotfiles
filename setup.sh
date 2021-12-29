ca1133097d04c26aa56173917a8a77385ea720d4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-29 22:44:06 +0100

    setup.sh: Changes
    * Use absolute path
    * Don't hardcode list of files and dirs
    
    Change-Id: Ie4b248c1dacc5f702b2ecb9026a30a5bfd84b404
    
    Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@
 # Setup all my stuff (scripts, configs, etc.)
 #
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
 GIT_USER_NAME="Sebastiano Barezzi"
 GIT_USER_EMAIL="barezzisebastiano@gmail.com"
 
@@ -24,12 +26,12 @@ packageinstall() {
 }
 
 # Copy every file from the repo to the current home folder
-for file in .bashrc-overlay .gitignore .xprofile README.md setup.sh; do
-	cp "${file}" ~/
+for file in $(ls -Ap "${SCRIPT_DIR}" | grep -v /); do
+	cp "${SCRIPT_DIR}/${file}" ~/
 done
 
-for dir in .fonts .git .ssh bin; do
-	cp -R "${dir}" ~/
+for dir in $(ls -Ap "${SCRIPT_DIR}" | grep /); do
+	cp -R "${SCRIPT_DIR}/${dir}" ~/
 done
 
 # Source .bashrc additions
@@ -48,5 +50,5 @@ git config --global user.email "${GIT_USER_EMAIL}"
 # Add the Gerrit Change-id hook
 mkdir -p ~/.git/hooks
 git config --global core.hooksPath ~/.git/hooks
-cp commit-msg ~/.git/hooks/commit-msg
+cp "${SCRIPT_DIR}/commit-msg" ~/.git/hooks/commit-msg
 chmod u+x ~/.git/hooks/commit-msg
